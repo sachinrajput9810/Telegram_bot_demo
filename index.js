0450ddeb31c5a1dd4780f699f40321c4396c7acb
Sachin Rajput <165654923+sachinrajput9810@users.noreply.github.com>
2024-10-02 15:43:12 +0530

    telegram bot && Api key integrated && used axios for http && commands

---
+++ index.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf('7560685887:AAEVGRM-qX8EW4Zg-VhdnanEOQy0TALodGM')
+
+bot.start( (ctx) => ctx.reply("Ajjo biddu log in the house"))
+
+bot.command("weather" , (ctx) => ctx.reply("This is about the weather"))
+bot.command("whomadethis" , ctx => ctx.reply("sachu bhai"))
+
+bot.command("sum" , async (ctx) => {
+    const response = await axios.get("https://raw.githubusercontent.com/sachinrajput9810/Leetcode_POTD/refs/heads/main/sum_of_prefix_scores.java")
+    // console.log(response.data)
+    return ctx.reply(response.data)
+} )
+bot.on('sticker' , ctx => ctx.reply('❤️'))
+
+bot.launch()
